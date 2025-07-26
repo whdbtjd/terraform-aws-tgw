1af34205d810119e0393e43b4282c267d8482062
whdbtjd <cys990617@gmail.com>
2025-07-26 17:35:54 +0900

    DynamoDB생성

--- init.tf
+++ init.tf
@@ -6,3 +6,13 @@ resource "aws_s3_bucket" "tfstate" {
     bucket = "cloudwave-terraform-tgw"
 }
 
+resource "aws_dynamodb_table" "terraform_state_lock" {
+    name = "terraform-lock-tgw"
+    hash_key = "LockID"
+    billing_mode = "PAY_PER_REQUEST"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
