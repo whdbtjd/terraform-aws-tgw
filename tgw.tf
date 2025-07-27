c91a0fa55c2e66adbd9fdd0e6ea4ae6b00b371b0
whdbtjd <cys990617@gmail.com>
2025-07-27 20:08:32 +0900

    tgw 각 vpc의 서브넷과 연결

--- tgw.tf
+++ tgw.tf
@@ -5,3 +5,33 @@ resource "aws_ec2_transit_gateway" "tgw" {
         Name = "terraform-tgw"
     }
 }
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-vpc-a" {
+    subnet_ids         = [aws_subnet.subnet-a.id]
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+    vpc_id             = aws_vpc.vpc-a.id
+
+    tags = {
+        Name = "tgw-vpc-a"
+    }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-vpc-b" {
+    subnet_ids         = [aws_subnet.subnet-b.id]
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+    vpc_id             = aws_vpc.vpc-b.id
+
+    tags = {
+        Name = "tgw-vpc-b"
+    }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-vpc-c" {
+    subnet_ids         = [aws_subnet.subnet-c.id]
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+    vpc_id             = aws_vpc.vpc-c.id
+
+    tags = {
+        Name = "tgw-vpc-c"
+    }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "subnet_names" {
   type    = list(string)
   default = ["subnet-a", "subnet-b", "subnet-c"]
 }
+
+variable "vpc_names" {
+  type    = list(string)
+  default = ["vpc-a", "vpc-b", "vpc-c"]  
+}
