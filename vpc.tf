847908b82db76320410c9fc4d56670393f3a9c71
whdbtjd <cys990617@gmail.com>
2025-07-27 14:14:29 +0900

    subnet a 생성

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,11 @@ resource "aws_vpc" "vpc-a" {
     cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "subnet-a" {
+    vpc_id = aws_vpc.vpc-a.id
+    cidr_block = "10.0.1.0/24"
+}
+
 resource "aws_vpc" "vpc-b" {
     cidr_block = "172.16.0.0/16"
 }
