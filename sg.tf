9672d14b886b3434ee459c27e37702799169fc0b
whdbtjd <cys990617@gmail.com>
2025-07-27 21:05:15 +0900

    b, c인스턴스에 ping 테스트를 위한 icmp 인바운드 규칙 추가

--- sg.tf
+++ sg.tf
@@ -29,3 +29,16 @@ resource "aws_vpc_security_group_ingress_rule" "tgw_sg_ingress" {
     security_group_id = aws_security_group.tgw_sg[each.key].id
     cidr_ipv4         = each.value == aws_vpc.vpc-a.cidr_block ? "0.0.0.0/0" : each.value
 }
+
+resource "aws_vpc_security_group_ingress_rule" "tgw_sg_icmp" {
+  for_each = {
+    "vpc_b" = aws_vpc.vpc-b.cidr_block
+    "vpc_c" = aws_vpc.vpc-c.cidr_block
+  }
+
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  cidr_ipv4         = aws_vpc.vpc-a.cidr_block     
+  security_group_id = aws_security_group.tgw_sg[each.key].id
+}
