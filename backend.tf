a1da4e56a8b4d9af2b5426705d4d446e6357632f
whdbtjd <cys990617@gmail.com>
2025-07-27 15:12:00 +0900

    backend.tf 생성 후 환경 초기화

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket          = "cloudwave-terraform-tgw"
+    key             = "terraform.tfstate"
+    region          = "ap-northeast-2"
+    encrypt         = true
+    dynamodb_table  = "terraform-lock-tgw"
+  }
+
+}
