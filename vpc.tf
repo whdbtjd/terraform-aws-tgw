e83bf729852feec285f9644c593adb3f0e5998c5
whdbtjd <cys990617@gmail.com>
2025-07-27 14:22:01 +0900

    subnet-b생성

--- vpc.tf
+++ vpc.tf
@@ -2,15 +2,20 @@ resource "aws_vpc" "vpc-a" {
     cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet-a" {
-    vpc_id = aws_vpc.vpc-a.id
-    cidr_block = "10.0.1.0/24"
-}
-
 resource "aws_vpc" "vpc-b" {
     cidr_block = "172.16.0.0/16"
 }
 
 resource "aws_vpc" "vpc-c" {
     cidr_block = "192.168.0.0/16"
+}
+
+resource "aws_subnet" "subnet-a" {
+    vpc_id = aws_vpc.vpc-a.id
+    cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "subnet-b" {
+    vpc_id = aws_vpc.vpc-b.id
+    cidr_block = "172.16.20.0/24"
 }
