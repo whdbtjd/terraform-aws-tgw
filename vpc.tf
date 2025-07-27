a6086b26c040ee41fe459e362df6158bd768f24f
whdbtjd <cys990617@gmail.com>
2025-07-27 14:32:21 +0900

    vpc-a 라우팅 테이블 생성

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,4 @@
+# vpc생성
 resource "aws_vpc" "vpc-a" {
     cidr_block = "10.0.0.0/16"
 }
@@ -10,6 +11,7 @@ resource "aws_vpc" "vpc-c" {
     cidr_block = "192.168.0.0/16"
 }
 
+# 각 vpc에 서브넷 생성
 resource "aws_subnet" "subnet-a" {
     vpc_id = aws_vpc.vpc-a.id
     cidr_block = "10.0.1.0/24"
@@ -24,3 +26,9 @@ resource "aws_subnet" "subnet-c" {
     vpc_id = aws_vpc.vpc-c.id
     cidr_block = "192.168.20.0/24"
 }
+
+# 각 vpc에 라우팅 테이블 생성
+resource "aws_route_table" "rt-a" {
+    vpc_id = aws_vpc.vpc-a.id
+}
+
